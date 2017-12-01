dbc120eabc7fb02c1eaeb99df07da0f4c9c2b37c
Darren-MSI <yangyu030000@gmail.com>
2017-12-01 14:40:50 -0800

    add new test

--- social-robot-V2/index.js
+++ social-robot-V2/index.js
@@ -52,7 +52,7 @@ function handleDeviceMotionEvent(event) {
 
     var acceleration = Math.sqrt(accX * accX + accY * accY + accZ * accZ);
 
-    if (parseFloat(acceleration).toFixed(1) > parseFloat("3").toFixed(1)) {
+    if (parseFloat(acceleration).toFixed(1) > parseFloat("6").toFixed(1)) {
         speak_auch();
     }
 
